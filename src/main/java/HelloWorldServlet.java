f29ac1c332a4f4f00bb89d261146688a8b74c62b
SUNG IL LEE <sungil.lee90@gmail.com>
2020-06-02 13:50:49 -0500

    complete Hello World Servlet exercise.

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,24 @@
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
+//    Create a page that displays "Hello World!" when a user visits /hello
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
-
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        res.setContentType("text/html");
+        PrintWriter out = res.getWriter();
+        out.println("<h1>Hello, World!</h1>");
     }
 }
+
+//    Create a page that displays a number that goes up by one every time the /count page is viewed.
+@WebServlet(name = "CountsPageViewd", urlPatterns = "/count")
+
+
+
+// http://localhost:8081/hello
